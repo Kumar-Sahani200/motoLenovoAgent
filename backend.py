e7fc2331154c1f26f85c6dfea78c92e6314170da
Kumar Sahani <kumarsahani1311@gmail.com>
2025-07-24 18:06:59 +0530

    created Get() to handle read req on the root

--- backend.py
+++ backend.py
@@ -9,6 +9,15 @@ class RequestState(BaseModel):
 
 app = FastAPI()
 
+
+@app.get("/")
+def read_root():   
+    """Root endpoint to check if the server is running"""
+    return {"message": "Welcome to the MotoLeno Agent API!",
+            "description": "This API provides information about Motorola and Lenovo devices.",
+            "api endpoint": "https://motolenovoagent.onrender.com/docs", 
+            "git repository": "https://github.com/Kumar-Sahani200/motoLenovoAgent"}
+
 @app.post("/chat")
 def chat(request: RequestState):
     """API endpoint to handle chat requests"""
@@ -16,6 +25,7 @@ def chat(request: RequestState):
     res=getAgentRes(request.query)
 
     return res
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=10000)
